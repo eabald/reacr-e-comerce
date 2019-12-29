79d736c94e995a42bdfee7f9f1ca17cb61b7529e
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2019-12-29 22:21:11 +0100

    added checkout page and checkout item component and selectors from reselect

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React, { Component } from 'react'
 import { Switch, Route, Redirect } from "react-router-dom"
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect";
 
 import './App.css'
 
 import HomePage from './pages/hompage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component'
 import Header from './components/header/header.component'
+
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends Component {
 
@@ -40,6 +44,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
@@ -57,8 +62,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispachToProps = dispatch => ({
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
@@ -8,21 +9,19 @@ import { ReactComponent as ShopingIcon } from "../../assets/shopping-bag.svg"
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden, itemCount }) => {
-  console.log(itemCount)
-  return (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShopingIcon className='shopping-icon' />
     <span className='item-count'>{itemCount}</span>
   </div>
-)}
+)
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
