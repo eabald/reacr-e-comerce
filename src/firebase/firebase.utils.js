de5424662bd9ffca4f64c7782e39aa8c73eb462e
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2020-03-01 11:54:41 +0100

    added sagas for user and cart reducers

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -64,13 +64,22 @@ export const convertCollectionsSnapshotToMap = (collecions) => {
   }, {})
 }
 
+export const getCurrentUser = () => {
+  return new Promise((reslove, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe()
+      reslove(userAuth)
+    }, reject)
+  })
+}
+
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
-const provider = new firebase.auth.GoogleAuthProvider()
-provider.setCustomParameters({ prompt: 'select_account' })
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+googleProvider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider)
 
 export default firebase
